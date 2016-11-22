16abd4b8cb9f6b0bd01a0750fd0e4f25f0dd84d3
Dan Colon <decolon13@gmail.com>
2016-11-21 21:47:29 -0800

    Finished bio object

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,18 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var bio = {
+	name: "Peter Parker",
+	role: "Pizza Delivery Man",
+	contacts: {
+		mobile: "111-111-1111",
+		email: "spider@man.com",
+		github: "https://github.com/peterparker/resume",
+		twitter: "https://twitter.com/spiderman",
+		location: "20 Ingram Street, Queens, NY 11375"
+	}
+	welcomeMessage: "Hello and welcome to my fantastic resume",
+	skills: "Science, web slinging, witty banter, fighting crime",
+	biopic: "http://i.annihil.us/u/prod/marvel/i/mg/2/00/53710b14a320b.png",
+	display: function(){
+
+	}
+}
+
